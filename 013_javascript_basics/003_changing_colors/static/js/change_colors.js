27f887da991c1fb64d8dd374867628038011b276
rpsteinbrueck <robsteinbruck@hotmail.co.uk>
2023-12-14 17:41:11 +0100

    added 03_changing_colors js

---
+++ 013_javascript_basics/003_changing_colors/static/js/change_colors.js
@@ -0,0 +1,7 @@
+let change_backgroundcolor = function (color) {
+    document.getElementById("bodycolor").style.backgroundColor = color;
+};
+
+let change_textcolor = function (color) {
+    document.getElementById("bodycolor").style.color = color;
+};
