43d65ca58cec7f19563368459abe5f48960734c3
rpsteinbrueck <robsteinbrueck@hotmail.co.uk>
2024-01-23 00:11:15 +0100

    still buggy but kind of working

--- 014_jquery/04_dynamic_list_jquery/static/js/dynamic_list_jquery.js
+++ 014_jquery/04_dynamic_list_jquery/static/js/dynamic_list_jquery.js
@@ -1,111 +1,68 @@
 let itemList = [];
+let checkboxList = [];
 
 if (typeof Cookies.get("cookie_itemList") != "undefined") {
     itemList = Cookies.get("cookie_itemList").split(",");
+
+    if (typeof Cookies.get("cookie_checkboxList") != "undefined") {
+        checkboxList = Cookies.get("cookie_checkboxList").split(",");
+    }
 }
 
-//if (localStorage.getItem("itemList".split(","))) {}
-
 const prependNewItem = function (item_id, item_name) {
+    let filteredCheckboxList = checkboxList.filter(function (item) {
+        return item.toLowerCase() == item_name.toLowerCase(); 
+    });
+
+    if (filteredCheckboxList.length) {
+        checkboxStatus = 'checked="true"'
+    } else {
+        checkboxStatus = ""
+    }
+
+    
     $("#item_list").prepend(`<div class="form-check" data_item_id="${item_id}">
-<input class="form-check-input" type="checkbox" value="${item_name}" id="item_${item_id}">
-<label class="form-check-label" for="flexCheckDefault" checked=false id="item_${item_id}">
-${item_name}
-</label>
-<button
-class="btn btn-danger"
-type="button"
-data_item_id="${item_id}"
-onclick="rmItem('${item_id}')">
-Delete
-</button>
-</div>`);
+                                <input class="form-check-input" 
+                                       type="checkbox"
+                                       ${checkboxStatus}
+                                       value="${item_name}" 
+                                       id="item_${item_id}">
+                                <label class="form-check-label"
+                                       for="flexCheckDefault" 
+                                       checked=false 
+                                       id="item_${item_id}">${item_name}
+                                </label>
+                                <button class="btn btn-danger"
+                                        type="button"
+                                        data_item_id="${item_id}"
+                                        onclick="rmItem('${item_id}')">Delete
+                                </button>
+                             </div>`
+                            );
 };
 
 const createItemList = function () {
-    if ( itemList !== 0) {
-        $(itemList).each(function (item_id, item_name) {
-            prependNewItem(item_id, item_name);
-        });
-    }
+    $(itemList).each(function (item_id, item_name) {
+        prependNewItem(item_id, item_name);
+    });
 };
 
-
-
-
-
 const addItem = function () {
-
     let value = $("#new_item").val();
 
     let filteredList = itemList.filter(function (item) {
-        return item.toLowerCase() == value.toLowerCase();
+        return item.toLowerCase() == value.toLowerCase(); 
     });
 
     if (!filteredList.length) {
         itemList.push($("#new_item").val());
         Cookies.set("cookie_itemList", itemList, { expires: 365 });
-        //localStorage.setItem("cookie_itemList", JSON.stringify(itemList));
         prependNewItem(itemList.length - 1, itemList[itemList.length - 1]);
     } else {
         $("#new_item").val("");
     }
     $("#new_item").val("").focus();
 };
-
-$("#add_item").on("click", addItem);
-$("#new_item").on("keyup", function (e) {
-    if (e.keyCode == 13) {
-        addItem();
-    }
-});
-
-if (itemList.length !== 0) {
-    createItemList();
-}
-
-
-const fuzzyFinder = function (filteredList) {
-    $("#item_list").empty();
-    $(filteredList).each(prependNewItem);
-};
-
-const filterList = function () {
-    let value = $(this).val().toLowerCase();
-
-    let filteredList = itemList.filter(function (item) {
-        return item.toLowerCase().includes(value);
-    });
-    fuzzyFinder(filteredList);
-};
-
-$("#new_item").on("keyup", filterList);
-$("#add_item").on("click", filterList);
-
-/*$("input.form-check-input").on("click", function () {
-    let checkbox = $(this);
-
-    console.log(checkbox.prop("checked"));
-
-    if (checkbox.prop("checked") == true) {
-        // save in array
-    }
-});*/
-
-$("[data_item_id]").each(function (index, item) {
-    let element = $(this);
-
-    if (element.attr("data_item_id" == 1)) {
-        element.find("input.form-check-input").prop("checked", true);
-        //$("[data_item_id]").find("input.form-check-input").prop("checked", true)
-    }
-});
-
-/*$("input.form-check-input").each(function (index, input) {
-    if (input.attr("id") == "data_item" + 2) {
-        $(input).prop("checked", true);
-    }
-});*/
 
 function rmItem(value) {
 	if (! value in itemList) {
@@ -130,6 +87,70 @@ function rmItem(value) {
         createItemList()
     } else {
         Cookies.remove("cookie_itemList")
+        Cookies.remove("cookie_checkboxList")
     }
 	return true
 }
+
+const fuzzyFinder = function (filteredList) {
+    $("#item_list").empty();
+    $(filteredList).each(prependNewItem);
+};
+
+const filterList = function () {
+    let value = $(this).val().toLowerCase();
+
+    let filteredList = itemList.filter(function (item) {
+        return item.toLowerCase().includes(value);
+    });
+    fuzzyFinder(filteredList);
+};
+
+if (itemList.length !== 0) {
+    createItemList();
+}
+
+$("#add_item").on("click", addItem);
+$("#new_item").on("keyup", function (e) {
+    if (e.keyCode == 13) {
+        addItem();
+    }
+});
+
+$("#new_item").on("keyup", filterList);
+$("#add_item").on("click", filterList);
+
+$("input.form-check-input").on("click", function () {
+    let checkbox = $(this);
+
+    if (checkbox.prop("checked") !== true) {
+
+        for (let count = 0; count < checkboxList.length; count++) {
+            if (checkboxList[count] === $(checkbox).val()) {
+                delete checkboxList[count];;
+            }
+        }
+
+        let checkboxArr = []
+        for (let count = 0; count < checkboxList.length; count++) {
+            if (checkboxList[count] !== undefined) {
+                checkboxArr.push(checkboxList[count]);
+            }
+        }
+        checkboxList = checkboxArr
+        if (checkboxList.length !== 0) {    
+            Cookies.set("cookie_checkboxList", checkboxList, { expires: 365 });
+        } else {
+            Cookies.remove("cookie_checkboxList")
+        }
+    } else {
+        let filteredCheckboxList = checkboxList.filter(function (item) {
+            return item.toLowerCase() == $(checkbox).val().toLowerCase(); 
+        });
+
+        if (!filteredCheckboxList.length) {
+            checkboxList.push($(checkbox).val())
+            Cookies.set("cookie_checkboxList", checkboxList, { expires: 365 });
+        }
+    }
+});
