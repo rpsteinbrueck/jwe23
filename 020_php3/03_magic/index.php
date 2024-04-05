fb3dfaaa0806dd8e749761988906e025b513fb3a
rpsteinbrueck <robsteinbruck@hotmail.co.uk>
2024-04-05 19:00:09 +0200

    more php3 classes and magic classes

---
+++ 020_php3/03_magic/index.php
@@ -0,0 +1,27 @@
+<?php
+
+error_reporting(E_ALL);
+
+include "magic_class.php";
+
+$m = new magic();
+$m->firstname = "Maria";
+$m->surname = "Stellar";
+
+echo "<pre>";
+print_r($m);
+echo "</pre>";
+
+echo "<br>";
+
+echo $m->firstname;
+echo "<br>";
+echo $m->surname;
+echo "<br>";
+
+
+$m->save("user01", 5, true, "some random parameter");
+
+
+
+?>
---
+++ 020_php3/03_magic/magic_class.php
@@ -0,0 +1,30 @@
+<?php
+
+# do not use this type of class
+class magic {
+
+    # saves all properties over __set() that do not exist
+    private array $data = array();
+
+    # if a property gets set which isn't defined __set()-magic-method will be used
+    public function __set($property, $value) {
+        $this->data[$property] = $value;
+    }
+
+    # if a property is set which wasn't initially defined use __get()-magic-method to retrieve value
+    public function __get($property) {
+        return $this->data[$property];
+    }
+
+    # if a method has been called which does not exist
+    public function __call($method, $parameter) {
+        echo "This method was called " . $method . "!";
+        echo "<pre>";
+        print_r($parameter);
+        echo "</pre>";
+    }
+
+}
+
+
+?>
